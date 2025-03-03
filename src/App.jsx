dc3db800274b40d72bd003d9b692ee242935be4b
JanLan <idiomasjanlan@gmail.com>
2025-03-02 22:32:14 -0300

    fo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
+import React from 'react';
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import HomeContainer from './components/HomeContainer'
 import ContactContainer from './components/ContactContainer'
 import ProductDetail from './components/ProductDetail'
-import Cart from './components/Cart'
+import CartWithBoundary from './components/Cart'
 import CheckoutForm from './components/CheckoutForm'
 import ErrorBoundary from './components/ErrorBoundary'
-import CartProvider from './context/CartContext'
-// Importa la configuración de Firebase
+import { CartProvider } from './context/CartContext'
+
 import './firebaseConfig'
 
 function App() {
@@ -23,7 +24,7 @@ function App() {
               <Route exact path="/category/:catId"element={<HomeContainer/>}/>
               <Route exact path="/contact"element={<ContactContainer/>}/>
               <Route exact path="/product/:id"element={<ProductDetail/>}/>
-              <Route exact path="/cart"element={<Cart/>}/>
+              <Route exact path="/cart"element={<CartWithBoundary/>}/>
               <Route exact path="/checkout"element={<CheckoutForm/>}/>
             </Routes>
           </BrowserRouter>
